812c0877939a77507c0ecbfae493d7f68ae5ec90
rjcodebytes <136474535+rjcodebytes@users.noreply.github.com>
2024-03-01 16:11:44 +0530

    Update rc2.c

--- rc2.c
+++ rc2.c
@@ -11,19 +11,19 @@ int main() {
         perror("fork failed");
         exit(EXIT_FAILURE);
     } else if (id == 0) {
-        // Child process
+    
         printf("Child process executing ls command:\n");
         system("ls");
         exit(EXIT_SUCCESS);
     } else {
-        // Parent process
-        wait(NULL); // Wait for the child process to finish
+        
+        wait(NULL); 
 
         printf("Parent process executing ps command:\n");
         char *const ps_args[] = {"ps", NULL};
         execv("/bin/ps", ps_args);
 
-        // execv will only return if an error occurs
+        
         perror("execv failed");
         exit(EXIT_FAILURE);
     }
